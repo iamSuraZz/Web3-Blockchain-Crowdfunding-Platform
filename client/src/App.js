a1c39786686d3a57d603064130989e4329214c9b
SuraZz007 <surajdev9886@gmail.com>
2022-12-16 19:09:45 +0530

    add sidebar component in the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import {
+  Home,
+  Profile,
+  CreateCampaign,
+  CampaignDetails,
+} from "./pages/index.js";
+import { Navbar, Sidebar } from "./components/index";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
